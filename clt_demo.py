1615c384a00729e99cebed8b4aa3668c7744e81a
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-15 16:17:16 +0000

    Hello world

--- clt_demo.py
+++ clt_demo.py
@@ -1,3 +1,12 @@
 #############################
 # Central limit theorum app #
 #############################
+
+import streamlit as st
+
+st.set_page_config(page_title="Central Limit Theorum",
+                    page_icon=":bar_chart:",
+                    layout="wide"
+                    )
+
+st.write('Hello World')
