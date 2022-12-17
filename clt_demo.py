6c1504aa6b09b01de89dbb848a973c68784d37f1
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-17 22:28:26 +0000

    update to plotly

--- clt_demo.py
+++ clt_demo.py
@@ -5,7 +5,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.express as px
+import plotly_express as px
 
 binom_dist = np.random.binomial(1, .5, 1000)
 list_of_means = []
