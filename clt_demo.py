8ad283d67663d035c49ee2da29e2c38548591f3f
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-17 21:59:53 +0000

    plotly chart insert

--- clt_demo.py
+++ clt_demo.py
@@ -4,13 +4,19 @@
 
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly_express as px
 
 binom_dist = np.random.binomial(1, .5, 1000)
 list_of_means = []
 
 for i in range(0, 1000):
      list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
-fig, ax = plt.subplots()
-ax = plt.hist(list_of_means)
-st.pyplot(fig)
+
+
+fig_score_by_course = px.bar(
+    list_of_means,
+    x=list_of_means.index,
+    y="distrubtion",
+    orientation="v",
+    template="plotly_dark",
+)
